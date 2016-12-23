30eaf077c748b28425278adb24c614ade69918e3
aisino-liqq <liquanqiang@aisino.com>
2016-12-23 18:11:29 +0800

    md5

--- src/com/carl/encrypt/MD5.java
+++ src/com/carl/encrypt/MD5.java
@@ -1,8 +1,9 @@
 package com.carl.encrypt;
 
-import java.math.BigInteger;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.UUID;
 
 import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
@@ -26,8 +27,11 @@ public class MD5 {
 
 	/**
 	 * 对文本进行32位小写MD5加密
-	 * @param plainText 要进行加密的文本
-	 * @param charset 底层字节编码格式
+	 * 
+	 * @param plainText
+	 *            要进行加密的文本
+	 * @param charset
+	 *            底层字节编码格式
 	 * @return 十六进制字符串形式的MD5摘要，长度32位
 	 */
 	public static String textToMD5L32(String plainText, String charset) {
@@ -63,8 +67,11 @@ public class MD5 {
 
 	/**
 	 * 对文本进行32位大写MD5加密
-	 * @param plainText 要进行加密的文本
-	 * @param charset 底层字节编码格式
+	 * 
+	 * @param plainText
+	 *            要进行加密的文本
+	 * @param charset
+	 *            底层字节编码格式
 	 * @return 十六进制字符串形式的MD5摘要，长度32位
 	 */
 	public static String textToMD5U32(String plainText, String charset) {
@@ -75,11 +82,44 @@ public class MD5 {
 		return result.toUpperCase();
 	}
 
+	/**
+	 * md5 加盐 每次保存密码到数据库时，都生成一个随机4位码(截取uuid的前4位)，将这4位数字和密码相加再求MD5摘要，
+	 * 然后在摘要中再将这4位数字按规则掺入形成一个36位的字符串。在验证密码时再从36位字符串中按规则提取4位数字，和用户输入的密码相加再MD5。
+	 * 按照这种方法形成的结果肯定是不可直接反查的，且同一个密码每次保存时形成的摘要也都是不同的。 
+	 * 中国  md5:C13DCEABCB143ACD6C9298265D618A9F  
+	 * salt:ada9
+	 * 中国ada9 md5:1410D5082BAF798AC7363B076EDCDF43
+	 * 加盐的md5结果插入盐:起始位0，第5，11，18，26位分别顺序插入4位盐值的一位
+	 *             1410DA5082BADF798AC7A363B076E9DCDF43
+	 * 
+	 * @param password
+	 */
+	public static String textToMD5U36WithSalt(String password, String charset) {
+		String salt = UUID.randomUUID().toString().substring(0, 4);
+		log.info("salt:" + salt);
+		password = textToMD5L32(password + salt, charset);
+		log.info("加salt后md5:" + password.toUpperCase());
+		password.substring(0, 8);
+		StringBuffer sb = new StringBuffer();
+		sb.append(password.substring(0, 5)).append(salt.charAt(0))
+		.append(password.substring(5, 11)).append(salt.charAt(1))
+		.append(password.substring(11, 18)).append(salt.charAt(2))
+		.append(password.substring(18, 26)).append(salt.charAt(3)).append(password.substring(26));
+		return sb.toString().toUpperCase();
+	}
+
+	public boolean checkMd5Salt(String password,String md5code){
+		
+		return true;
+	}
+	
+	
 	@Test
 	public void testMd5encrypt() {
 		try {
-			System.out.println(textToMD5L32("中国", "UTF-8"));
-			System.out.println(textToMD5U32("中国", "UTF-8"));
+			log.info(textToMD5L32("中国", "UTF-8"));
+			log.info(textToMD5U32("中国ada9", "UTF-8"));
+			log.info(textToMD5U36WithSalt("中国", "UTF-8"));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
